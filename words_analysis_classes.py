83206272ca47a614bce6b2392a66390e8bd6afef
[Gemma-Rate] <thebrickmaster@theratefamily.co.uk>
2019-10-07 00:51:18 +0100

    Added word tokenisation

--- words_analysis_classes.py
+++ words_analysis_classes.py
@@ -3,8 +3,8 @@ Classes for analysing prose itself.
 """
 import nltk
 from nltk.corpus import wordnet as wn
-import key_functions as kf
-import gui_windows as gu
+import re
+import log
 
 # Highlighting.
 # Nearby word similarity.
@@ -35,9 +35,9 @@ class WordSet(object):
 
     def __init__(self, text):
         self.raw = text
-        self.token = kf.word_token(text, no_punctuation=None, lower=None)
+        self.token = self.word_token(text, no_punctuation=None, lower=None)
         # Keeps in punctuation and upper cases.
-        self.processed = kf.word_token(text)
+        self.processed = self.word_token(text)
         self.pos = None
         self.word_colours = {'VB':'blue', 'VBD':'blue', 'VBG':'blue',
                              'VBN':'blue', 'VBP':'blue', 'VBZ':'blue',
@@ -74,4 +74,33 @@ class WordSet(object):
         ----------
         """
         pass
+
+    @log.log_function
+    def word_token(self, data, no_punctuation=True, lower=True):
+
+        """
+        Strips out the punctuation and tokenises words in the input string
+        data.
+
+        Parameters
+        -----------
+        data : string
+        String containing data to be tokenised.
+
+        Returns
+        -------
+        datatoken : list
+        List of tokenised data.
+
+        """
+
+        if no_punctuation:
+            data = re.sub(r"[^\w\s]", ' ', data)
+            # Remove punctuation.
+        if lower:
+            datatoken = nltk.word_tokenize(data.lower())
+        else:
+            datatoken = nltk.word_tokenize(data)
+
+        return datatoken
 
